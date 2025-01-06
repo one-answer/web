c46093cbff63965ae201e4386b2cbff133dab3f4
awwr <114227169+awwr@users.noreply.github.com>
2025-01-06 20:53:48 +0900

    Edit button

--- style/pList.ts
+++ style/pList.ts
@@ -14,6 +14,9 @@ export default function (props: PListProps) {
                     <div class="post-meta">
                         <span class="author">${item.username}</span>
                         <span class="date" time_stamp="${item.create_date}"></span>
+                        ${(props.i && props.i.uid == item.uid) ? html`
+                            <a class="edit" href="/edit/-${item.pid}">编辑</a>
+                        `: ''}
                         <a class="reply" href="/edit/${item.pid}">回复</a>
                     </div>
                 </div>
