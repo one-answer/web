47bd9ddb5b0f10e5826b5534a91152dcfb9f2492
awwr <114227169+awwr@users.noreply.github.com>
2025-01-16 04:07:05 +0900

    p count

--- route/pList.ts
+++ route/pList.ts
@@ -1,9 +1,10 @@
 import { Context } from "hono";
 import { BaseProps, DB, Post, Thread, User } from "./base";
 import { Auth, Pagination } from "./core";
-import { asc, eq, or, getTableColumns, aliasedTable, and } from 'drizzle-orm';
+import { asc, eq, or, getTableColumns, and, sql } from 'drizzle-orm';
 import { raw } from "hono/html";
 import pListView from "../style/pList";
+import { alias } from "drizzle-orm/sqlite-core";
 
 export interface PListProps extends BaseProps {
     topic: { [x: string]: any; }
@@ -22,10 +23,9 @@ export default async function (a: Context) {
     )?.[0]
     if (!topic) { return a.notFound() }
     const page = parseInt(a.req.param('page') ?? '0') || 1
-    const pagination = Pagination(20, topic.posts, page, 2)
     const uid = parseInt(a.req.query('uid') ?? '0')
-    const QuotePost = aliasedTable(Post, 'QuotePost')
-    const QuoteUser = aliasedTable(User, 'QuoteUser')
+    const QuotePost = alias(Post, 'QuotePost')
+    const QuoteUser = alias(User, 'QuoteUser')
     const data = await DB
         .select({
             ...getTableColumns(Post),
@@ -34,6 +34,7 @@ export default async function (a: Context) {
             gid: User.gid,
             quote_content: QuotePost.content,
             quote_username: QuoteUser.username,
+            count: sql`COUNT() OVER()`,
         })
         .from(Post)
         .where(and(
@@ -46,6 +47,7 @@ export default async function (a: Context) {
         .orderBy(asc(Post.tid), asc(Post.pid))
         .offset((page - 1) * 20)
         .limit(20)
+    const pagination = Pagination(20, data ? (data?.[0]?.count as number ?? 0) : 0, page, 2)
     const title = raw(topic.subject)
     return a.html(pListView({ a, i, topic, page, pagination, data, title }));
 }
