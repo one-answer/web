527804dbe3973c15ba7534425a8b5ad4061ad42a
axww <114227169+awwr@users.noreply.github.com>
2025-03-13 03:23:02 +0900

    Message: 将已加载消息标记为已读

--- app/mList.ts
+++ app/mList.ts
@@ -1,8 +1,9 @@
 import { Context } from "hono";
 import { DB, Message, Post, User, Thread } from "./base";
 import { Auth } from "./core";
-import { and, asc, eq, inArray, lt } from 'drizzle-orm';
+import { and, asc, eq, gt, inArray } from 'drizzle-orm';
 import { alias } from "drizzle-orm/sqlite-core";
+import { mRead } from "./mCore";
 
 export async function _mList(a: Context) {
     const i = await Auth(a)
@@ -14,7 +15,7 @@ export async function _mList(a: Context) {
         .select({
             type: Message.type,
             time: Message.time,
-            pid: Post.pid,
+            pid: Message.pid,
             content: Post.content,
             uid: Post.uid,
             name: User.name,
@@ -27,13 +28,19 @@ export async function _mList(a: Context) {
         .where(and(
             eq(Message.uid, i.uid),
             inArray(Message.type, type),
-            time ? lt(Message.time, time) : undefined,
+            time ? gt(Message.time, time) : undefined,
         ))
         .leftJoin(Post, eq(Post.pid, Message.pid))
         .leftJoin(QuotePost, eq(QuotePost.pid, Post.quote_pid))
         .leftJoin(User, eq(User.uid, Post.uid))
         .leftJoin(Thread, eq(Thread.tid, Post.tid))
         .orderBy(asc(Message.time))
-        .limit(20)
+        .limit(10)
+    // 将已加载消息标记为已读
+    data.forEach(function (row) {
+        if (row.type > 0) {
+            mRead(i.uid, row.type, row.time, row.pid)
+        }
+    })
     return a.json(data)
 }
