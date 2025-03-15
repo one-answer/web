e650f35b13f742ca9a896da5b60ff3152ccb91a1
axww <114227169+awwr@users.noreply.github.com>
2025-03-15 15:21:12 +0900

    加载更多消息

--- bare/MList.ts
+++ bare/MList.ts
@@ -6,34 +6,40 @@ export function MList(z: Props) {
     return html`
 ${Header(z)}
 
-<button onclick="mClear()">【全部标记已读】</button>
+<button onclick="mClear()">【清空消息】</button>
 <div id="list"></div>
-<a href="javascript:;" onclick="mLoad();"></a>
+<a href="javascript:;" onclick="mFetch();">【加载更多】</a>
 
 <script>
+let pid = 0;
 async function mClear() {
     const response = await fetch('/_mClear');
-    if(response.ok){ location.reload(); }
+    if (response.ok) {
+        document.getElementById('list').innerHTML = '';
+    }
 }
 async function mFetch() {
     try {
-        const response = await fetch('/_mList?type=1');
+        const response = await fetch('/_mList?type=1&pid='+pid);
         const data = await response.json();
-        let html = '';
+        if (data.length) {
+            pid = data.at(-1).post_pid;
+            console.log(pid)
+        } else {
+            alert('没有更多通知了');
+        }
         data.forEach(function(row){
-            html += '<div class="message" pid="'+row.post_pid+'">';
-            html += '<i style="color:grey;font-size:10px;">'+row.quote_content+'</i><br />';
-            html += '<a href="/p/'+row.post_pid+'" target="_blank">';
-            html += '<b>'+row.post_name+'</b>: ';
-            html += ''+row.post_content+'<br />';
-            html += '</div><hr />'
+            document.getElementById('list').innerHTML += '<div class="message" pid="'+row.post_pid+'">';
+            document.getElementById('list').innerHTML += '<i style="color:grey;font-size:10px;">'+row.quote_content+'</i><br />';
+            document.getElementById('list').innerHTML += '<a href="/p/'+row.post_pid+'" target="_blank">';
+            document.getElementById('list').innerHTML += '<b>'+row.post_name+'</b>: ';
+            document.getElementById('list').innerHTML += ''+row.post_content+'<br />';
+            document.getElementById('list').innerHTML += '</div><hr />'
         });
-        document.getElementById('list').innerHTML = html;
     } catch (error) {
         console.error("获取数据失败:", error);
     }
 }
-let mLast = 0;
 mFetch();
 </script>
 
