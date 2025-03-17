4c6f39a272da8dd260d169e304b644a9877b355b
axww <114227169+awwr@users.noreply.github.com>
2025-03-18 00:41:03 +0900

    规整化

--- app/mList.ts
+++ app/mList.ts
@@ -4,6 +4,7 @@ import { MList } from "../bare/MList";
 
 export async function mList(a: Context) {
     const i = await Auth(a)
+    if (!i) { return a.text('401', 401) }
     const title = '消息'
     return a.html(MList({ a, i, title }));
 }
